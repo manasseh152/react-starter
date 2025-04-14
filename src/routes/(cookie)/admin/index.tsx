6db9d2fdf40f4f11cd846e68fa0ec9112b295b69
Manassseh Stam <manassseh2424@gmail.com>
2025-04-14 17:21:56 +0200

    remove cookie and no-cookie routes; add forms route with form and output sections

--- src/routes/(cookie)/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(cookie)/admin/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(cookie)/admin/"!</div>
-}
