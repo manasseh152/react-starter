3c0dc2dcefc765b285460bd27616656bfedcc4ee
Manasseh Stam <manasseh2424@gmail.com>
2025-05-07 16:38:40 +0200

    feat(i18n): add English and Dutch localization for edit instructions in home and translation files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,25 @@
 import appLogo from "@/assets/images/app-logo.svg";
 import { createFileRoute } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
 
 function App() {
+  const { t } = useTranslation("home");
+
   return (
-    <header className="w-full h-full flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+    <header className="w-full h-full flex flex-col items-center justify-center dark:bg-[#282c34] dark:text-white text-[calc(10px+2vmin)]">
       <img
         src={appLogo}
         className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
         alt="logo"
       />
       <p>
-        Edit
-        {" "}
-        <code>src/routes/index.tsx</code>
-        {" "}
-        and save to reload.
+        {t("editInstructionPrefix")}
+        <code>{t("editInstructionHighlight")}</code>
+        {t("editInstructionSuffix")}
       </p>
     </header>
   );
